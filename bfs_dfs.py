93ed15629c719f8bf61cd89c483de6e1de21107d
Avy <gibraltar.av@gmail.com>
2024-11-11 19:56:44 +0800

    Added distance calculation to BFSearch

--- bfs_dfs.py
+++ bfs_dfs.py
@@ -2,24 +2,29 @@ from bst import BST
 
 #Input: tree: binary tree of nodes with tree.root being the starting node and each node has .left and .right, 
 #       data: data of node to be found
-#Output: Node where Node.data == input_data + visited list (traversal path)
+#Output: Node where Node.data == input_data 
+#        visited list (traversal path) 
+#        dict of distances from root
 def bfSearch(tree:BST, data:int):
     visited = []
     queue = [tree.root]
+    distance = {f"Node {tree.root.data}":0}
 
     while queue:
         vertex = queue.pop(0)
         visited.append(vertex)
 
         if vertex.data == data:
-            return vertex, visited
+            return vertex, visited, distance
         
         if vertex.left != None and vertex.left not in visited:
             queue.append(vertex.left)
+            distance[f"Node {vertex.left.data}"] = distance[f"Node {vertex.data}"] + 1
 
         if vertex.right != None and vertex.right not in visited:
             queue.append(vertex.right)
-    return None, visited
+            distance[f"Node {vertex.right.data}"] = distance[f"Node {vertex.data}"] + 1
+    return None, visited, distance
 
 #Input: tree: binary tree of nodes with tree.root being the starting node and each node has .left and .right, 
 #       data: data of node to be found
@@ -45,7 +50,8 @@ def dfSearch(tree:BST, data:int):
 if __name__ == "__main__":
     b = BST.create_Instance(3)   
 
-    node, lst = bfSearch(b, None)
+    node, lst, dist = bfSearch(b, None)
+    print(dist)
     for i in lst:
         print(f"{i}", end=", ")
 
